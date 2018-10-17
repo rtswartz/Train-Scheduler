a55bed25f5ac33d80332277e2153cf95f61519de
Worm6 <rtswartz6@gmail.com>
2018-10-17 16:08:17 -0500

    linked firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  //Initilize Firebase
+  var config = {
+    apiKey: "AIzaSyA5l3a_r27GJmITKrMXkK4P-Cq9gcOiWyA",
+    authDomain: "train-scheduler-e608e.firebaseapp.com",
+    databaseURL: "https://train-scheduler-e608e.firebaseio.com",
+    projectId: "train-scheduler-e608e",
+    storageBucket: "train-scheduler-e608e.appspot.com",
+    messagingSenderId: "1077859171222"
+  };
+firebase.initializeApp(config);
+database = firebase.database();
+});
